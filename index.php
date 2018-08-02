31727df7510476109efd4eaa65847118828fe093
PrideWay <zakharserkov@me.com>
2018-08-02 19:26:28 +0300

    Start

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Date: 02.08.2018
+ * Zakhar Serkov <zakharserkov@me.com>
+ */
+require 'config.php';
+require 'templates/functions.php';
+
+// eof
---
+++ templates/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Date: 02.08.2018
+ * Zakhar Serkov <zakharserkov@me.com>
+ */
+
+
+// eof
